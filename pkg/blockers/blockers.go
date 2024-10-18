5b2cf1e080b5a04a06638c4299caf1a0634ce1c2
Jean-Philippe Evrard <open-source@a.spamming.party>
2024-10-18 23:02:06 +0200

    Remove unnecessary functions
    Blockers became part of main and delaytick is not used anymore.
    
    Signed-off-by: Jean-Philippe Evrard <open-source@a.spamming.party>

--- pkg/blockers/blockers.go
+++ pkg/blockers/blockers.go
@@ -1,21 +1,5 @@
 package blockers
 
-import (
-	"fmt"
-)
-
-// RebootBlocked checks that a single block Checker
-// will block the reboot or not.
-func RebootBlocked(blockers ...RebootBlocker) (blocked bool, blockernames []string) {
-	for _, blocker := range blockers {
-		if blocker.IsBlocked() {
-			blocked = true
-			blockernames = append(blockernames, fmt.Sprintf("%s", blocker))
-		}
-	}
-	return
-}
-
 // RebootBlocker interface should be implemented by types
 // to know if their instantiations should block a reboot
 // As blockers are now exported as labels, the blocker must
