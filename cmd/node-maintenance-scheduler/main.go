c5fda7b43100ecaa37c2a66344e6deb622c107fa
Jean-Philippe Evrard <open-source@a.spamming.party>
2025-11-05 10:15:24 +0100

    New iteration

--- cmd/node-maintenance-scheduler/main.go
+++ cmd/node-maintenance-scheduler/main.go
@@ -3,126 +3,127 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"log/slog"
+	"net/http"
 	"os"
 	"time"
 
 	"github.com/kubereboot/kured/internal/cli"
 	"github.com/kubereboot/kured/internal/maintenances"
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	cronlib "github.com/robfig/cron/v3"
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/client-go/informers"
-	"k8s.io/client-go/tools/cache"
+	"k8s.io/apimachinery/pkg/labels"
+	kubeinformers "k8s.io/client-go/informers"
+	"k8s.io/klog/v2"
 )
 
 var (
-	version = "unreleased"
+	version         = "unreleased"
+	activeSelectors map[string]labels.Selector // global map of active maintenance window selectors
 )
 
 func init() {
-	prometheus.MustRegister(maintenances.UnderMaintenanceGauge)
-}
-
-//type nodeOps interface {
-//	ListNodes(ctx context.Context, selector string) ([]corev1.Node, error)
-//	GetNode(ctx context.Context, nodeName string) (*corev1.Node, error)
-//	SetNodeCondition(ctx context.Context, nodeName string, condType string, status corev1.ConditionStatus, reason string) error
-//}
-//
-//type realNodeOps struct {
-//	client *kubernetes.Clientset
-//}
-//
-//func (r *realNodeOps) ListNodes(ctx context.Context, selector string) ([]corev1.Node, error) {
-//	lo := metav1.ListOptions{}
-//	if selector != "" {
-//		lo.LabelSelector = selector
-//	}
-//	list, err := r.client.CoreV1().Nodes().List(ctx, lo)
-//	if err != nil {
-//		return nil, err
-//	}
-//	return list.Items, nil
-//}
-//
-//func (r *realNodeOps) GetNode(ctx context.Context, nodeName string) (*corev1.Node, error) {
-//	return r.client.CoreV1().Nodes().Get(ctx, nodeName, metav1.GetOptions{})
-//}
-//
-//func (r *realNodeOps) SetNodeCondition(ctx context.Context, nodeName string, condType string, status corev1.ConditionStatus, reason string) error {
-//	now := metav1.NewTime(time.Now())
-//	cond := corev1.NodeCondition{
-//		Type:               corev1.NodeConditionType(condType),
-//		Status:             status,
-//		LastTransitionTime: now,
-//		Reason:             reason,
-//	}
-//	return retry.RetryOnConflict(retry.DefaultRetry, func() error {
-//		latest, err := r.client.CoreV1().Nodes().Get(ctx, nodeName, metav1.GetOptions{})
-//		if err != nil {
-//			return err
-//		}
-//		found := false
-//		conds := latest.Status.Conditions
-//		for i := range conds {
-//			if string(conds[i].Type) == condType {
-//				conds[i] = cond
-//				found = true
-//				break
-//			}
-//		}
-//		if !found {
-//			conds = append(conds, cond)
-//		}
-//		latest.Status.Conditions = conds
-//		_, err = r.client.CoreV1().Nodes().UpdateStatus(ctx, latest, metav1.UpdateOptions{})
-//		return err
-//	})
-//}
-
-func handleNodeDeletion(obj interface{}, globalQueue *maintenances.Queue) {
-	globalQueue.Dequeue(obj.(*corev1.Node).Name)
-}
-
-func handleNodeUpdate(obj interface{}, globalQueue *maintenances.Queue) {
-	node := obj.(*corev1.Node).DeepCopy()
-	if node.DeletionTimestamp != nil {
-		handleNodeDeletion(obj, globalQueue)
-	}
-	// If we don't know, we should not queue the node
-	if !hasCondition(node, "NeedsReboot", corev1.ConditionTrue) {
-		globalQueue.Dequeue(node.Name)
-	} else {
-		// explicitly in need of reboot, queue this
-		globalQueue.Enqueue(node)
-		// Only apply prometheus change if the node is in maintenance window!
-	}
+	prometheus.MustRegister(maintenances.ActiveMaintenanceWindowGauge)
 }
 
 func main() {
 	var (
-		debug                                                   bool
-		kubeconfig, cmPrefix, namespace, metricsHost, logFormat string
-		maxRebootConcurrency, metricsPort                       int
-		period                                                  time.Duration
+		debug                           bool
+		logFormat                       string
+		kubeconfig, cmPrefix, namespace string
+		metricsHost                     string
+		metricsPort                     int
+		period                          time.Duration
 	)
 	flag.StringVar(&cmPrefix, "config-prefix", "kured-maintenance-", "maintenance configmap prefix")
+	flag.DurationVar(&period, "period", time.Minute, "controller resync period")
 	flag.BoolVar(&debug, "debug", false, "Enable debug logging")
 	flag.StringVar(&kubeconfig, "kubeconfig", "", "optional kubeconfig")
 	flag.StringVar(&logFormat, "log-format", "json", "use text or json log format")
-	flag.IntVar(&maxRebootConcurrency, "max-reboot-concurrency", 10, "maximum amount of nodes in maintenance at any point in time")
-	flag.IntVar(&metricsPort, "metrics-port", 8080, "port number where metrics will listen")
 	flag.StringVar(&metricsHost, "metrics-host", "", "host where metrics will listen")
+	flag.IntVar(&metricsPort, "metrics-port", 8080, "port number where metrics will listen")
 	flag.StringVar(&namespace, "namespace", "kube-system", "Namespace where maintenance configmaps live")
-	flag.DurationVar(&period, "period", time.Minute, "period at which the main operations are done")
 
 	flag.Parse()
 
 	// Load flags from environment variables. Remember the prefix KURED_!
 	cli.LoadFromEnv()
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
 
+	// set up signals so we handle the shutdown signal gracefully
+	ctx := cli.SetupSignalHandler()
+
+	logger := NewLogger(debug, logFormat)
+	// For all the old calls using logger
+	slog.SetDefault(logger)
+	cronLogger := &cronSlogAdapter{logger}
+
+	//var windows []*maintenances.Window
+	//windows = append(windows, &maintenances.Window{
+	//	Name:         "maintenance-a",
+	//	Schedule:     "@every 2s",
+	//	Duration:     10 * time.Second,
+	//	NodeSelector: labels.Everything(),
+	//})
+	//windows = append(windows, &maintenances.Window{
+	//	Name:         "maintenance-b",
+	//	Schedule:     "@every 2s",
+	//	Duration:     1 * time.Second,
+	//	NodeSelector: labels.Everything(),
+	//})
+
+	client := cli.KubernetesClientSetOrDie("", kubeconfig)
+	windows := maintenances.LoadWindowsOrDie(ctx, client, namespace, cmPrefix)
+
+	slog.Info("Starting node-maintenance-scheduler",
+		"version", version,
+		"debug", debug,
+		"cmPrefix", cmPrefix,
+		"knownMaintenanceWindows", len(windows),
+	)
+
+	// Maintenance manager setup
+	c := cronlib.New(cronlib.WithLogger(cronLogger), cronlib.WithChain(cronlib.SkipIfStillRunning(cronLogger)))
+	loadAllCronJobs(ctx, c, windows, logger)
+	c.Start()
+
+	// Controller handling node events
+	kubeInformerFactory := kubeinformers.NewSharedInformerFactory(client, period)
+	controller := NewController(ctx, kubeClient, exampleClient,
+		kubeInformerFactory.Apps().V1().Deployments(),
+		exampleInformerFactory.Samplecontroller().V1alpha1().Foos())
+
+	// notice that there is no need to run Start methods in a separate goroutine. (i.e. go kubeInformerFactory.Start(ctx.done())
+	// Start method is non-blocking and runs all registered informers in a dedicated goroutine.
+	kubeInformerFactory.Start(ctx.Done())
+
+	if err = controller.Run(ctx, 2); err != nil {
+		logger.Error(err, "Error running controller")
+		klog.FlushAndExit(klog.ExitFlushTimeout, 1)
+	}
+
+	//nodes, _ := client.CoreV1().Nodes().List(ctx, metav1.ListOptions{})
+	//for _, node := range nodes.Items {
+	//	for _, w := range windows {
+	//		if w.IsActive(time.Now()) && w.ContainsNode(node.Labels) {
+	//			// node matches an active maintenance window
+	//			slog.Info("Node %s is under maintenance window %s\n", node.Name, w.Name)
+	//		}
+	//	}
+	//}
+	// Closes on Ctrl-C
+	http.Handle("/metrics", promhttp.Handler())
+	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", metricsHost, metricsPort), nil); err != nil {
+		slog.Error(fmt.Sprintf("unrecoverable error - failed to listen on metrics port: %v", err))
+		os.Exit(1)
+	} // #nosec G114
+	select {}
+
+}
+
+func NewLogger(debug bool, logFormat string) *slog.Logger {
 	var logger *slog.Logger
 	handlerOpts := &slog.HandlerOptions{}
 	if debug {
@@ -137,108 +138,46 @@ func main() {
 		logger = slog.New(slog.NewJSONHandler(os.Stdout, handlerOpts))
 		logger.Info("incorrect configuration for logFormat, using json handler")
 	}
-	// For all the old calls using logger
-	slog.SetDefault(logger)
-
-	client := cli.KubernetesClientSetOrDie(kubeconfig)
-	windows := maintenances.LoadWindowsOrDie(ctx, client, namespace, cmPrefix)
-
-	slog.Info("Starting node-maintenance-scheduler",
-		"version", version,
-		"debug", debug,
-		"cmPrefix", cmPrefix,
-		"rebootConcurrency", maxRebootConcurrency,
-		"metricsHost", metricsHost,
-		"metricsPort", metricsPort,
-		"knownMaintenanceWindows", len(windows),
-		"period", period,
-	)
-	slog.Info("You must reload this deployment in case of new or updated maintenance window(s)")
-
-	globalQueue := maintenances.NewQueue(maxRebootConcurrency)
-
-	// This is very lightweight.
-	// It might need to move to a sample-controller approach of having workqueues for large clusters.
-	// It needs to be tested first, as it is very simple.
-	factory := informers.NewSharedInformerFactory(client, 0)
-	informer := factory.Core().V1().Nodes().Informer()
-	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
-		AddFunc: func(obj interface{}) {
-			handleNodeUpdate(obj, globalQueue)
-		},
-		UpdateFunc: func(old, new interface{}) {
-			handleNodeUpdate(new, globalQueue)
-		},
-		DeleteFunc: func(obj interface{}) {
-			handleNodeDeletion(obj, globalQueue)
-		},
-	})
-
-	go informer.RunWithContext(ctx)
-
-	stopCh := make(chan struct{})
-	defer close(stopCh)
-
-	// Wait for informer caches to sync
-	if !cache.WaitForCacheSync(stopCh, informer.HasSynced) {
-		slog.Error("failed waiting for lease informer sync")
-		os.Exit(3)
-	}
-
-	go PutNodeInMaintainance(ctx, period, windows, globalQueue)
-
-	select {}
+	return logger
 }
 
-// PutNodeInMaintainance runs the maintenance loop.
-// It finds all the active window's nodes (matching their respective selectors), putting them in a new map (currentTickMap).
-// It compare the currentTickMap with the complete list of nodes, so that all the nodes absent of the currentTickMap are marked as "outside a maintenance" during this tick (prom under_maintenance=0).
-// TODO: Check if we can rely on the informer, or if that's feeling weird. If it's too slow with the informer, then make an explicit code path for that.
-// The nodes present in the currentTickMap are all in the window, but we do not know yet if they need Reboot or not.
-// For that, we have two sources of information: The global Pending queue and the node information.
-// We could use the lastTransition state to sort all those, but it's more tedious than checking the globalQueue's pending list.
-// So, we iterate over the globalQueue's pending list.
-// If the node is not in the currentTickMap, it needs reboot but cannot be under maintenance right now. Set the prom under_maintenance=0
-// If the node is in the currentTickMap, we double-check whether:
-//   - We can process the queue (max capacity)
-//     If we cannot, we break/skip the whole tick: Tick is full, so we need some node to finish its reboot first (and get dequeued).
-//   - The node is up to date in global Queue:
-//     Compare the node information in the currentTickMap with the global Queue -- by checking if the node is still in need of reboot (to avoid a race condition, for example, the node is just out of maintenance, but the informer didn't get the memo yet.)
-//     If not, dequeue it and continue for other nodes in the tick.
-//     If yes, we put the node in the active queue for maintenance.
-//     If successful, process the node by doing the api calls to patch the nodes.
-func PutNodeInMaintainance(ctx context.Context, period time.Duration, windows []*maintenances.Window, globalQueue *maintenances.Queue) {
-	ticker := time.NewTicker(period)
-	for now := range ticker.C {
-		_ = now // keep for potential time injection/tests
-		currentTickMap := map[string]*corev1.Node{}
-		for mwindowIdx := range windows {
-			mwindow := windows[mwindowIdx]
-			if !mwindow.IsActive(now) {
-				continue
-			}
-
-	}
-
+type cronSlogAdapter struct {
+	*slog.Logger
 }
 
-//ops := &realNodeOps{client: client}
+func (a *cronSlogAdapter) Info(msg string, keysAndValues ...interface{}) {
+	a.Logger.Info(msg, keysAndValues...)
+}
 
-//go WatchNodesConditions(ctx, ops, gs, &mu)
-//
-//ticker := time.NewTicker(10 * time.Second)
-//defer ticker.Stop()
-//for now := range ticker.C {
-//	_ = now // keep for potential time injection/tests
-//	mu.Lock()
-//	if err := enqueueCandidates(ctx, ops, windows, gs); err != nil {
-//		mu.Unlock()
-//		fmt.Fprintf(os.Stderr, "enqueue candidates failed: %v\n", err)
-//		continue
-//	}
-//	processQueue(ctx, ops, gs, maxRebootConcurrency)
-//	mu.Unlock()
-//}
+func (a *cronSlogAdapter) Error(err error, msg string, keysAndValues ...interface{}) {
+	// Prepend a key/value pair for the error.
+	// You can name the key whatever you want; "error" is conventional.
+	a.Logger.Error(msg, append([]any{"error", err.Error()}, keysAndValues...)...)
+}
+
+func loadAllCronJobs(ctx context.Context, c *cronlib.Cron, windows []*maintenances.Window, logger *slog.Logger) {
+	logger.Info("You must reload this deployment in case of new or updated maintenance window(s)")
+	for _, window := range windows {
+		window := window
+		_, err := c.AddFunc(window.Schedule, startWindow(window, logger))
+		if err != nil {
+			slog.Error("Problem adding function to schedule", "error", err.Error())
+		}
+	}
+}
+
+func startWindow(w *maintenances.Window, logger *slog.Logger) func() {
+	return func() {
+		logger.Info("Starting maintenance window", "window", w.Name)
+		maintenances.ActiveMaintenanceWindowGauge.WithLabelValues(w.Name).Set(1)
+		activeSelectors[w.Name] = w.NodeSelector
+
+		time.Sleep(w.Duration)
+		logger.Info("End of maintenance window", "window", w.Name)
+		maintenances.ActiveMaintenanceWindowGauge.WithLabelValues(w.Name).Set(0)
+		delete(activeSelectors, w.Name)
+	}
+}
 
 //
 //func enqueueCandidates(ctx context.Context, ops nodeOps, windows []*maintenances.Window, gs *globalState) error {
