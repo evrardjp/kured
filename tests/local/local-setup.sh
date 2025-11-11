ca9828d35942fdec4b5908f457b633d9d0ba2033
Jean-Philippe Evrard <open-source@a.spamming.party>
2025-11-11 08:25:13 +0100

    Next iteration still not good

--- tests/local/local-setup.sh
+++ tests/local/local-setup.sh
@@ -3,7 +3,8 @@
 kind create cluster --config local-kind.yaml
 kubectl apply -f ../../node-reboot-detector.yaml
 kubectl apply -f ../../node-reboot-reporter.yaml
-kubectl delete deployment -n kube-system node-reboot-reporter
+kubectl apply -f ../../node-maintenance-scheduler.yaml
 kubectl apply -f local-kind-manifests.yaml
 kind load docker-image ghcr.io/kubereboot/node-reboot-detector:dev  --name kured-local
 kind load docker-image ghcr.io/kubereboot/node-reboot-reporter:dev  --name kured-local
+kind load docker-image ghcr.io/kubereboot/node-maintenance-scheduler:dev  --name kured-local
