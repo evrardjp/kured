ab146ff23bfc79de00b2fdff21692d13c99d98cf
Jean-Philippe Evrard <open-source@a.spamming.party>
2025-11-12 17:41:30 +0100

    In progress

--- internal/conditions/comparison.go
+++ internal/conditions/comparison.go
@@ -3,27 +3,30 @@ package conditions
 import corev1 "k8s.io/api/core/v1"
 
 // Matches checks if node conditions meets the criteria defined by positive and negative conditions.
-// It returns true if the node has at least one positive condition set to True and none of the negative conditions set to True.
+// It returns true if the node has ALL required conditions set to True and none of the negative conditions set to True.
 // In other words: Negative conditions take precedence over positive conditions.
-func Matches(conditions []corev1.NodeCondition, positiveConditions, negativeConditions []string) bool {
-	// Negative Conditions take precedence over Positive Conditions
-	for _, condType := range negativeConditions {
+func Matches(conditions []corev1.NodeCondition, requiredConditionTypes, forbiddenConditionTypes []string) (bool, string) {
+	for _, condType := range forbiddenConditionTypes {
 		cond := getNodeCondition(conditions, condType)
 		// This block is not necessary. However, it makes the logic explicit: if a negative condition is not present, we ignore it.
 		if cond == nil {
 			continue
 		}
 		if cond.Status == corev1.ConditionTrue {
-			return false
+			return false, condType
 		}
 	}
-	for _, condType := range positiveConditions {
+	for _, condType := range requiredConditionTypes {
 		cond := getNodeCondition(conditions, condType)
-		if cond != nil && cond.Status == corev1.ConditionTrue {
-			return true
+		if cond == nil {
+			return false, condType
 		}
+		if cond.Status == corev1.ConditionFalse {
+			return false, condType
+		}
+
 	}
-	return false
+	return true, ""
 }
 
 func getNodeCondition(conditions []corev1.NodeCondition, condType string) *corev1.NodeCondition {
---
+++ internal/conditions/names.go
@@ -0,0 +1,21 @@
+// Package conditions provide a registry of well-known conditions.
+package conditions
+
+const (
+	// RebootRequiredConditionType is set up by node-reboot-reporter
+	RebootRequiredConditionType = "kured.dev/reboot-required"
+	// RebootRequiredConditionReason is the reason for the RebootRequired condition
+	RebootRequiredConditionReason = "KuredSentinelDetected"
+)
+const (
+	// UnderMaintenanceConditionType is set by maintenance-scheduler when a node matches an active maintenance window, but not necessarily undergoing maintenance yet.
+	UnderMaintenanceConditionType   = "kured.dev/under-maintenance-window"
+	UnderMaintenanceConditionReason = "KuredFoundMatchingMaintenanceWindowConfigmap"
+)
+
+const (
+	// InProgressMaintenanceConditionType is set by maintenance-scheduler when a node is undergoing maintenance (e.g., being rebooted).
+	InProgressMaintenanceConditionType                = "kured.dev/maintenance-in-progress"
+	InProgressMaintenanceConditionSuccessReason       = "KuredStartedMaintenance"
+	InProgressMaintenanceConditionBadConditionsReason = "KuredPrerequisitesConditionsUnmet"
+)
