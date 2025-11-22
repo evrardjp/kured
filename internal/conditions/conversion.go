80db06a69b933f66d76192c7984d36e2532aec87
Jean-Philippe Evrard <open-source@a.spamming.party>
2025-11-22 13:07:23 +0100

    Complete rewrite using controller-runtime

---
+++ internal/conditions/conversion.go
@@ -0,0 +1,42 @@
+package conditions
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func ConvertFromNodeConditions(nodeConditions []corev1.NodeCondition) *[]metav1.Condition {
+	var conditions []metav1.Condition
+	for _, nc := range nodeConditions {
+		conditions = append(conditions, metav1.Condition{
+			Type: string(nc.Type),
+			Status: map[corev1.ConditionStatus]metav1.ConditionStatus{
+				corev1.ConditionFalse:   metav1.ConditionFalse,
+				corev1.ConditionTrue:    metav1.ConditionTrue,
+				corev1.ConditionUnknown: metav1.ConditionUnknown,
+			}[nc.Status],
+			Reason:             nc.Reason,
+			Message:            nc.Message,
+			LastTransitionTime: nc.LastTransitionTime,
+		})
+	}
+	return &conditions
+}
+
+func ConvertToNodeConditions(metaConditions []metav1.Condition) *[]corev1.NodeCondition {
+	var conditions []corev1.NodeCondition
+	for _, mc := range metaConditions {
+		conditions = append(conditions, corev1.NodeCondition{
+			Type: corev1.NodeConditionType(mc.Type),
+			Status: map[metav1.ConditionStatus]corev1.ConditionStatus{
+				metav1.ConditionFalse:   corev1.ConditionFalse,
+				metav1.ConditionTrue:    corev1.ConditionTrue,
+				metav1.ConditionUnknown: corev1.ConditionUnknown,
+			}[mc.Status],
+			Reason:             mc.Reason,
+			Message:            mc.Message,
+			LastTransitionTime: mc.LastTransitionTime,
+		})
+	}
+	return &conditions
+}
